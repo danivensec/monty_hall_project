3f887a9b902c202377ff68ac0da34d182d709299
daniela <97327012+danivensec@users.noreply.github.com>
2026-03-27 15:56:00 -0700

    update monty_hall2.py

--- monty_hall2.py
+++ monty_hall2.py
@@ -1,19 +1,14 @@
 import random as rand 
 
 def monty_game_PARTONE (games, switch, stay): 
-    global goatCount
-    global carCount
-    global goatCount_stay
-    global carCount_stay
     
-   
+    totalCarCount = 0
     carCount_stay = 0 
     goatCount_stay = 0 
     carCount = 0 
     goatCount = 0 
    
     
-
 
     for i in range (games): 
         doors = [0,0,0] # the zero represents the goats, at the start, all the doors have a goat behind them 
@@ -24,7 +19,7 @@ def monty_game_PARTONE (games, switch, stay):
 
         
 
-        first_choice = rand.randint(0,2)
+        first_choice = rand.randint(0,2) #contestant first choice that is random 
 
         monty_open = [x for x in range(3) if x!= first_choice and doors[x]==0]
 
@@ -41,27 +36,32 @@ def monty_game_PARTONE (games, switch, stay):
 
             if doors[third_choice] == 1:
                 carCount += 1
+                totalCarCount+=1
                 
             else: 
                 goatCount += 1
         if stay == True: 
                 if doors[first_choice] == 1:
                      carCount_stay += 1
+                     totalCarCount+=1
                 
                 else: 
                      goatCount_stay += 1
     
-    switch_win_probability = (carCount/games)*100
-    stay_win_probability = (carCount_stay/games)*100
+    switch_win_probability = (carCount/totalCarCount)*100
+    stay_win_probability = (carCount_stay/totalCarCount)*100
 
-    print( "won", carCount, "times")
+    total_wins_probability = (totalCarCount/games)*100
+
+    print( "switch", carCount, "times")
 
     print ( "The probability that a contestant switches the door and wins in a simulation that loops 10000 time is: ", switch_win_probability, "%")
 
-    print( "won", carCount_stay, "times")
+    print( "stay", carCount_stay, "times")
 
     print ( "The probability that a contestant stays with the same door and wins in a simulation that loops 10000 time is: ", stay_win_probability, "%")
 
+    print("total probability of winnning the game", total_wins_probability)
 
 games= 10000
 
