80280aa32359a6e9f9a08ca33ff9527a17d4fce3
daniela <97327012+danivensec@users.noreply.github.com>
2026-03-27 22:50:51 -0700

    Update monty_hall2.py

--- monty_hall2.py
+++ monty_hall2.py
@@ -1,12 +1,12 @@
 import random as rand 
 
-def monty_game_PARTONE (games, switch, stay): 
+# Part 1: Simulating the game
+def monty_game_PART_ONE (games, switch, stay): 
     
-    totalCarCount = 0
-    carCount_stay = 0 
-    goatCount_stay = 0 
-    carCount = 0 
-    goatCount = 0 
+    carCount_stay = 0  #keep stack of the times it wins when it stays with the door
+    carCount_switch = 0 # will keep track of when it wins when it switches
+    stay_games = 0 # will keep track everytime it stays with the first choice
+    switch_games =0 # will keep track of when it switches choices
    
     
 
@@ -15,15 +15,135 @@ def monty_game_PARTONE (games, switch, stay):
 
         #randomly replace a goat with a car
         car = rand.randint(0,2)
+        doors[car] = 1 #1 is for car 
+
+        
+
+        first_choice = rand.randint(0,2) # randomly choose a door
+        
+        #the host will open one door that is not the players first choice and does not have the car behind it
+        monty_open = [x for x in range(3) if x!= first_choice and doors[x]==0] 
+        monty_choice = rand.choice(monty_open)
+        
+        #randomly chooses if it is going to stay or switch
+        switch = rand.choice([True,False])
+        if switch == True: 
+            stay = False
+        else: 
+            stay = True
+
+        
+        #operations of staying with the first door or switching
+        if switch == True: #switch operation
+            switch_games +=1 
+            #will choose the next door, the one that was not open and the one that was not their inital choice
+            switch_choice = next(x for x in range(3) if x != first_choice and x!= monty_choice)
+            
+            #checks if the door has the car or not
+            if doors[switch_choice] == 1:
+                carCount_switch += 1
+            
+        if stay == True: #stay operation
+                stay_games+=1
+                #if inital choice has the car, the car count for stay is incremented
+                if doors[first_choice] == 1:
+                     carCount_stay += 1  
+
+
+    #probability calculations
+    switch_win_probability = (carCount_switch/switch_games)*100
+    stay_win_probability = (carCount_stay/stay_games)*100
+
+
+    print("The results of a 10000 simulation with 3 doors (traditional Monty Hall simulation):")
+    print("The probabilty of switching is ", switch_win_probability, "%")
+    print("The probabilty of staying is ", stay_win_probability, "%\n")
+
+
+
+#Part 2: Increasing the Number of doors
+def monty_game_PART_TWO(games, switch, stay): 
+    carCount_stay = 0  
+    carCount_switch = 0 
+    stay_games = 0
+    switch_games =0
+   
+    
+
+    for i in range (games): 
+        doors = [0,0,0,0,0,0,0,0,0,0] # the zero represents the goats, at the start, all the doors have a goat behind them 
+
+        #randomly replace a goat with a car
+        car = rand.randint(0,9)
         doors[car] = 1
 
         
 
-        first_choice = rand.randint(0,2) #contestant first choice that is random 
+        first_choice = rand.randint(0,9)
 
-        monty_open = [x for x in range(3) if x!= first_choice and doors[x]==0]
+        #will open 8 doors with goats behind them
+        monty_available = [x for x in range(10) if x!= first_choice and doors[x]==0 ]
+        monty_choices = rand.sample(monty_available,8)
 
+        #track the remaining door
+        remaining_door = [x for x in range (10) if x!= first_choice and x not in monty_choices]
+
+        switch = rand.choice([True,False])
+        if switch == True: 
+            stay = False
+        else: 
+            stay = True
+
+        if switch == True: # when the player does decide to switch to the remaing door
+            switch_games+=1
+            switch_choice = remaining_door[0]
+
+            if doors[switch_choice] == 1:
+                carCount_switch += 1
+
+        if stay == True: 
+                stay_games+=1
+                if doors[first_choice] == 1:
+                     carCount_stay += 1
+
+    switch_win_probability = (carCount_switch/switch_games)*100
+    stay_win_probability = (carCount_stay/stay_games)*100
+
+
+    print("The results of a 10000 simulation with 10 doors and the host open 8 doors:")
+    print("The probabilty of switching is ",switch_win_probability,"%")
+    print("The probabilty of staying is ",stay_win_probability, "%\n")
+
+
+#Part 3: Increasing number of door chocies
+def monty_game_PART_THREE(games, switch, stay): 
+    
+    carCount_stay = 0 
+    carCount_switch = 0
+    switch_games = 0
+    stay_games = 0 
+    
+   
+    
+
+    for i in range (games): 
+        doors = [0,0,0,0,0,0,0,0,0,0] # the zero represents the goats, at the start, all the doors have a goat behind them 
+
+        #randomly replace a goat with a car
+        car = rand.randint(0,9)
+        doors[car] = 1
+
+        
+
+        first_choice = rand.randint(0,9)
+
+        #only one door is open
+        monty_open = [x for x in range(10) if x!= first_choice and doors[x]==0]
         monty_choice = rand.choice(monty_open)
+
+        #a list of the remaining doors is created
+        remaining_doors = ([x for x in range (10) if x!= first_choice and x != monty_choice])
+        
 
         switch = rand.choice([True,False])
         if switch == True: 
@@ -32,37 +152,37 @@ def monty_game_PARTONE (games, switch, stay):
             stay = True
 
         if switch == True:
-            third_choice = next(x for x in range(3) if x != first_choice and x!= monty_choice)
+            switch_games += 1
+            switch_choice_of_door = rand.choice(remaining_doors) # will randomly choose a new door from the remaing list
 
-            if doors[third_choice] == 1:
-                carCount += 1
-                totalCarCount+=1
-                
-            else: 
-                goatCount += 1
+            if doors[switch_choice_of_door] == 1:
+                carCount_switch += 1
+            
         if stay == True: 
+                stay_games +=1
                 if doors[first_choice] == 1:
                      carCount_stay += 1
-                     totalCarCount+=1
-                
-                else: 
-                     goatCount_stay += 1
+
+    switch_win_probability = (carCount_switch/switch_games)*100
+    stay_win_probability = (carCount_stay/stay_games)*100
+
     
-    switch_win_probability = (carCount/totalCarCount)*100
-    stay_win_probability = (carCount_stay/totalCarCount)*100
 
-    total_wins_probability = (totalCarCount/games)*100
+    print("The results of a 10000 simulation with 10 doors but the host only opens one door:")
+    print("The probabilty of switching is ",switch_win_probability,"%")
+    print("The probabilty of staying is ",stay_win_probability,"%\n")
 
-    print( "switch", carCount, "times")
+   
 
-    print ( "The probability that a contestant switches the door and wins in a simulation that loops 10000 time is: ", switch_win_probability, "%")
 
-    print( "stay", carCount_stay, "times")
 
-    print ( "The probability that a contestant stays with the same door and wins in a simulation that loops 10000 time is: ", stay_win_probability, "%")
 
-    print("total probability of winnning the game", total_wins_probability)
+
 
 games= 10000
+default_bool = None
+monty_game_PART_ONE(games,default_bool, default_bool)
 
-monty_game_PARTONE(games,None, None)
+monty_game_PART_TWO(games,default_bool, default_bool)
+
+monty_game_PART_THREE(games,default_bool, default_bool)
